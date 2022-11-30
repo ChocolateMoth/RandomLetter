88f6f1e4c1bb8484e35cc8398c9503db9cf5a081
4400114616 <drschokolade1@gmail.com>
2022-11-30 14:34:49 -0700

    Add project files.

---
+++ random letter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;// dont forget
+
+namespace random_letter
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int b = 0;
+            while (b < 30)
+            {
+                string charc = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";//alphabet that i took from the internet so I dont have to write it all out.
+                Random rnd = new Random();//random num
+                int i = rnd.Next(0, charc.Length);//0 to lenght of alph
+
+
+                var letter = new List<char>();
+                letter.Add(charc[i]);
+                Console.Write(charc[i]+" ");//write
+                b++; // looks cool without this
+            }
+
+            
+        }
+        
+    }
+}
