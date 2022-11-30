c85004f8d93f437f0b2804980c29c42f19295c50
4400114616 <drschokolade1@gmail.com>
2022-11-30 14:51:52 -0700

    SALES GONE DOWN THE [[drain]] [drain]

--- random letter/Program.cs
+++ random letter/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;// dont forget
+using System.Collections.Generic;// dont forget . . .                                                                                                                                                 i'm with you in the dark
 
 namespace random_letter
 {
@@ -14,13 +14,30 @@ namespace random_letter
                 string charc = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";//alphabet that i took from the internet so I dont have to write it all out.
                 Random rnd = new Random();//random num
                 int i = rnd.Next(0, charc.Length);//0 to lenght of alph
+                
+                // just remember that charc is the alphabet string. I already see myself forgetting this
 
 
                 var letter = new List<char>();
                 letter.Add(charc[i]);
                 Console.Write(charc[i]+" ");//write
+
+
+                if(b >= 29)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("-- Original array -- ");
+                    foreach(var e in letter)
+                    {
+                        Console.Write($" {e}");
+                    }
+                }
+
                 b++; // looks cool without this
+
+                
             }
+             // how to get final list from while loop ?
 
             
         }
